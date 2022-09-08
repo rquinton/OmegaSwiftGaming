cfa4522a886c962116d57f70597c24fd4286ba51
Raphael <raphael.quinton90@gmail.com>
2022-09-08 09:09:44 -0600

    Changes

--- src/App.js
+++ src/App.js
@@ -6,43 +6,67 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-         Welcome to Omega Swift Gaming
+        OMEGA SWIFT GAMING
         </h1>
       </header>
-      <h2>
-          Game Download Links
-      </h2>
-      <h3>
-        <a href="https://themilkywayapp.com">Milky Way</a>
-      </h3>
-      <h3>
-        <a href="https://highrollerdownload.com/">High Roller</a>
-      </h3>
-      <h3>
-        <a href="http://www.gameroom777.com/">Gameroom Online</a>
-      </h3>
-      <h3>
-        <a href="https://playlol.online:81/web-mobile/?v=1005">Life of Luxury</a>
-      </h3>
-      <h3>
-        <a href="https://pandamaster.vip:8888/index.html">Panda master</a>
-      </h3>
-      <h3>
-        <a href="http://firekirin.xyz:8580/index.html">Fire Kirin</a>
-      </h3>
-      <h3>
-        <a href="http://orionstars.vip:8580/index.html">Orion Stars</a>
-      </h3>
-      <h3>
-        <a href="http://www.oceanking99.net/">X-Game</a>
-      </h3>
-      <h3>
-        <a href="https://www.ultramonster.net/">Ultra monster</a>
-      </h3>
-      <h3>
-        <a href="https://www.vpower777.com/#/download/index">V Power</a>
-      </h3>
-      
+      <div id="page-body">
+        <div className="body">
+
+          <p id="heading">
+            Please stay connected with us through any of the following channels!
+          </p>
+          
+          <div className="channels">
+            <a href="https://m.me/rquinton90" target="_blank" rel="noreferrer" className='messenger'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Facebook_Messenger_logo_2020.svg/128px-Facebook_Messenger_logo_2020.svg.png" alt="aa"></img></a>
+            <a href="https://www.facebook.com/OmegaSwiftGaming" target="_blank" rel="noreferrer" className='facebook-page'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/F_icon.svg/128px-F_icon.svg.png" alt=""></img></a>
+            <a href="https://t.me/OmegaSwiftGaming" target="_blank" rel="noreferrer" className='telegram'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/128px-Telegram_logo.svg.png" alt=""></img></a>
+            <a href="https://wa.me/qr/DB5KENPJII5VL1" target="_blank" rel="noreferrer" className='whatsapp'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/WhatsApp_logo-color-vertical.svg/128px-WhatsApp_logo-color-vertical.svg.png" alt=""></img></a>
+            <a href="tel:+13036163342" className='textmessage'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/IMessage_logo.svg/128px-IMessage_logo.svg.png" alt=""></img></a>
+            <a href="mailto:omegaswiftonline@gmail.com" className='gmail'><img src="https://upload.wikimedia.org/wikipedia/commons/2/2e/Gmail_2020.png" alt=""></img></a>
+          </div>
+
+          <div className="wrapper">
+            <h2>Available Platforms</h2>
+            <h1>Omega Swift Gaming</h1>
+            <h3>(Click to Download)</h3>
+            <ul>
+              <li>
+                <a className="button4" href="https://themilkywayapp.com" target="_blank" rel="noreferrer">Milky Way</a>
+              </li>
+              <li>
+                <a className="button4" href="https://highrollerdownload.com/" target="_blank" rel="noreferrer">High Roller</a>
+              </li>
+              <li>
+                <a className="button4" href="http://www.gameroom777.com/" target="_blank" rel="noreferrer">Gameroom Online</a>
+              </li>
+              <li>
+                <a className="button4" href="https://playlol.online:81/web-mobile/?v=1005" target="_blank" rel="noreferrer">Life of Luxury</a>
+              </li>
+              <li>
+                <a className="button4" href="https://pandamaster.vip:8888/index.html" target="_blank" rel="noreferrer">Panda Master</a>
+              </li>
+              <li>
+                <a className="button4" href="http://firekirin.xyz:8580/index.html" target="_blank" rel="noreferrer">Fire Kirin</a>
+              </li>
+              <li>
+                <a className="button4" href="http://orionstars.vip:8580/index.html" target="_blank" rel="noreferrer">Orion Stars</a>
+              </li>
+              <li>
+                <a className="button4" href="http://www.oceanking99.net/" target="_blank" rel="noreferrer">X Game</a>
+              </li>
+              <li>
+                <a className="button4" href="https://www.ultramonster.net/" target="_blank" rel="noreferrer">Ultra Monster</a>
+              </li>
+              <li>
+                <a className="button4" href="https://www.vpower777.com/#/download/index" target="_blank" rel="noreferrer">V Power</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          All Rights Reserved | Omega Swift Gaming 	&copy; 2022
+        </div>
+      </div>
     </div>
   );
 }
